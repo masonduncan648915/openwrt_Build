c9c41378118a13b454009b0f0d97517a08b17087
masonduncan648915 <105209750+masonduncan648915@users.noreply.github.com>
2023-03-04 05:14:11 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,4 +8,4 @@ sed -i 's/192.168.1.1/192.168.3.1/g' openwrt/package/base-files/files/bin/config
 #sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
 #sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
 git clone https://github.com/masonduncan648915/openwrt-packages.git package/openwrt-packages
-git clone https://github.com/masonduncan648915/small.git package/openwrt-packages
+git clone https://github.com/masonduncan648915/small.git package/small
